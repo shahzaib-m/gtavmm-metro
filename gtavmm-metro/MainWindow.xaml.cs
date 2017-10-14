f6493eab9e1b7c53ed98e7d84e7c70b20d4b0e6c
Shahzaib Mirani <shahzaib6874@gmail.com>
2017-10-14 23:16:36 +0100

    Implemented ScriptMods DataGrid

--- gtavmm-metro/MainWindow.xaml.cs
+++ gtavmm-metro/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Threading;
 using System.Windows.Media.Animation;
 using System.Reflection;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
+using gtavmm_metro.Models;
 
 namespace gtavmm_metro
 {
@@ -25,6 +27,8 @@ namespace gtavmm_metro
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        ObservableCollection<Script_Mod> ScriptMods;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -40,6 +44,18 @@ namespace gtavmm_metro
             // perform all tasks (check files, load mods, etc.) here while the user is shown a progress ring
             await Task.Delay(1000);     // temp substitute for work delay
 
+            
+            ScriptMods = new ObservableCollection<Script_Mod>();
+            ScriptMods.Add(new Script_Mod { Id = 1, Name = "MP Vehicles", Description = "Allows MP vehicles to be spawned in SP", IsEnabled = true });
+            ScriptMods.Add(new Script_Mod { Id = 2, Name = "Turbo Boost", Description = "NFS like turbo boost for vehicles", IsEnabled = false });
+            ScriptMods.Add(new Script_Mod { Id = 1, Name = "MP Vehicles", Description = "Allows MP vehicles to be spawned in SP", IsEnabled = true });
+            ScriptMods.Add(new Script_Mod { Id = 2, Name = "Turbo Boost", Description = "NFS like turbo boost for vehicles", IsEnabled = false });
+            ScriptMods.Add(new Script_Mod { Id = 1, Name = "MP Vehicles", Description = "Allows MP vehicles to be spawned in SP", IsEnabled = true });
+            ScriptMods.Add(new Script_Mod { Id = 2, Name = "Turbo Boost", Description = "NFS like turbo boost for vehicles", IsEnabled = false });
+            ScriptMods.Add(new Script_Mod { Id = 1, Name = "MP Vehicles", Description = "Allows MP vehicles to be spawned in SP", IsEnabled = true });
+            ScriptMods.Add(new Script_Mod { Id = 2, Name = "Turbo Boost", Description = "NFS like turbo boost for vehicles", IsEnabled = false });
+
+            this.ScriptModsDataGrid.ItemsSource = ScriptMods;
 
             await Task.Run(() => UserInteractionStartNow());    // enable the UI for the user when tasks finished.
         }
@@ -68,6 +84,12 @@ namespace gtavmm_metro
 
                 return String.Format("GTAV Mod Manager Metro {0}.{1}.{2}", major, minor, build);
             }
+        }
+
+        private void EditScriptModButton_Click(object sender, RoutedEventArgs e)
+        {
+            Script_Mod chosen_mod = ((FrameworkElement)sender).DataContext as Script_Mod; // the sender Script_Mod object from the datagrid
+            MessageBox.Show("Sender Name: " + chosen_mod.Name);
         }
     }
 }
---
+++ gtavmm-metro/Models/Script_Mod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtavmm_metro.Models
+{
+    class Script_Mod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
